34ea7903fec452dda81beaeb6c6bbb3130c0ab81
ry4njs <ryanjstrickland04@gmail.com>
2025-04-06 14:52:06 -0400

    Add function to perform one of four operations (+,-,*,/) on two numbers

--- script.js
+++ script.js
@@ -16,3 +16,22 @@ function divide(num1, num2) {
     }
     return num1 / num2;
 }
+
+let num1 = 3;
+let operator = '+';
+let num2 = 5;
+
+function operate(operator, num1, num2) {
+    switch (operator) {
+        case '+':
+            return add(num1, num2);
+        case '-':
+            return substract(num1, num2);
+        case '*':
+            return multiply(num1, num2);
+        case '/':
+            return divide(num1, num2);
+        default:
+            return "Error: unknown operator";
+    }
+}
